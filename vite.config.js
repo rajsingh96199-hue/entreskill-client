9c5473a90ce2a8820d6291bf74e53dae6850d89f
rajsingh96199-hue <rajsingh96199.in>
2026-03-22 12:45:49 +0530

    Add axios dependency

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (warning.code === 'UNUSED_EXTERNAL_IMPORT') return
-        warn(warning)
-      }
-    }
-  }
 })
